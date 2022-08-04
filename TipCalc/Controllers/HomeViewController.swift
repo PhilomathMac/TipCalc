f52ea893f69ab0774ff64553116626cf892c9427
KenzieMac <kenziemacd@icloud.com>
2022-08-04 10:18:45 -0700

    - Link outlets and actions
    NOTE: Error with ResultsViewController not having an initializer

--- TipCalc/Controllers/HomeViewController.swift
+++ TipCalc/Controllers/HomeViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    @IBOutlet weak var billTextField: UITextField!
+    
+    @IBOutlet weak var tipSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var splitLabel: UILabel!
+    
+    @IBOutlet weak var splitStepper: UIStepper!
+    @IBOutlet weak var roundingToggle: UISwitch!
+    
+    let calcBrain = CalcBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculateButtonClicked(_ sender: Any) {
+        
+    }
+    
 }
 
--- TipCalc/Controllers/ResultsViewController.swift
+++ TipCalc/Controllers/ResultsViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ResultsViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    let totalPerPerson: String
+    let settingsString: String
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,5 +31,8 @@ class ResultsViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    @IBAction func recalculateButtonClicked(_ sender: Any) {
+        
+    }
+    
 }
