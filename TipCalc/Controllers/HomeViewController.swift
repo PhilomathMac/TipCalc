cba2852b6a62f2dd285e0b8833ac886e5e94ec5d
KenzieMac <kenziemacd@icloud.com>
2022-08-04 10:14:57 -0700

    - Organize files into MVC

--- TipCalc/Controllers/HomeViewController.swift
+++ TipCalc/Controllers/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  TipCalc
 //
 //  Created by McKenzie Macdonald on 8/4/22.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ TipCalc/Models/CalcBrain.swift
@@ -0,0 +1,12 @@
+//
+//  CalcBrain.swift
+//  TipCalc
+//
+//  Created by McKenzie Macdonald on 8/4/22.
+//
+
+import Foundation
+
+struct CalcBrain {
+    
+}
